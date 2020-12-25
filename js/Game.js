a7f9c4799d7ffdc4d4ba673aed291d6c9cbda2f3
adameshg <72934694+adameshg@users.noreply.github.com>
2020-12-25 15:10:28 -0500

    Update to Phrase and Game classes

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,53 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+class Game {
+    constructor () {
+        this.missed = 0;
+        this.phrases = [
+            'May the force be with you', 
+            'ET phone home', 
+            'Life is like a box of chocolates', 
+            'Nobody puts baby in a corner', 
+            'I see dead people'
+        ];
+        this.activePhrase = null;
+    }
+
+    /**
+     * Hides start screen overlay and sets the random phrase.
+     */
+    startGame() {
+        document.getElementById('overlay').style.display = 'none';
+        this.activePhrase = getRandomPhrase(this.phrases);
+    }
+
+    /**
+     * Selects random phrase out of array of phrases.
+     *
+     * @param {array} arr Array of phrases.
+     * @return {object} Random phrase.
+     */
+    getRandomPhrase(arr) {
+        return arr[Math.floor(Math.random() * arr.length)].split('');
+    }
+
+    handleInteraction(e) {
+        if (e.target.tagName === 'BUTTON') {
+            e.target.className = 'chosen';
+            e.target.disabled = true;
+        }
+
+        // Add checkWin();
+    }
+
+    removeLife(e) {
+        const letterFound = this.checkLetter(e.target);
+        if (letterFound === null && e.target.tagName === 'BUTTON') {
+            const scoreboardImg = document.querySelectorAll('.tries img');
+            scoreboardImg[this.missed].src = 'images/lostHeart.png';
+            this.missed++;
+        }
+    }
+}
--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,61 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+
+const letter = document.getElementsByClassName('letter');
+
+class Phrase {
+    constructor(phrase) {
+        this.phrase = phrase.toLowerCase();
+    }
+
+    /**
+     * Adds letter placeholders to the display upon game start.
+     *
+     * @param {array} arr The secret phrase.
+     */
+    addPhraseToDisplay(arr) {
+        for (let i = 0; i < arr.length; i++) {
+            let li = document.createElement('li');
+            li.textContent = arr[i];
+            document.querySelector('#phrase ul').appendChild(li);
+
+            if (arr[i] === ' ') {
+                li.classList.add('space');
+            } else {
+                li.classList.add('letter');
+            }
+        }
+    }
+
+    /**
+     * Checks to see if the letter selected by the player matches a letter in the phrase.
+     *
+     * @param {object} etarget The clicked button.
+     * @return {boolean} Whether the letter represented by the clicked button matches 
+     * any of the letters in the phrase (true) or not (false).
+     */
+    checkLetter(etarget) {
+    let match = null; 
+    for (let i = 0; i < letter.length; i++) {
+            if (letter[i].textContent.toLowerCase() === etarget.textContent.toLowerCase()) {
+                match = true;
+            }
+        }
+        return match;
+    }
+
+    /**
+     * Reveals the letters on the board that match the player's selection.
+     *
+     * @param {object} etarget The clicked button.
+     */
+    showMatchedLetter(etarget) {
+        for (let i = 0; i < letter.length; i++) {
+            if (checkLetter(etarget)) {
+                letter[i].classList.remove('hide');
+                letter[i].classList.add('show');
+            }
+        }
+    }
+}
