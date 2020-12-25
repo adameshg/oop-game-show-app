5d67a7c0a79443688675ba4c1af81f5de3509939
adameshg <72934694+adameshg@users.noreply.github.com>
2020-12-25 18:31:32 -0500

    Add function 'addPhraseToDisplay'

--- js/Phrase.js
+++ js/Phrase.js
@@ -2,4 +2,23 @@
  * Project 4 - OOP Game App
  * Phrase.js */
 
- 
+ class Phrase {
+    constructor(phrase) {
+        this.phrase = phrase;
+    }
+
+    addPhraseToDisplay() {
+        const phraseList = document.querySelector('#phrase ul');
+
+        for (let char of this.phrase) {
+            const li = document.createElement('li');
+            li.textContent = char;
+            phraseList.appendChild(li);
+            if (char === ' ') {
+                li.className = 'space';
+            } else {
+                li.className = 'letter';
+            }
+        }
+    }
+ }
